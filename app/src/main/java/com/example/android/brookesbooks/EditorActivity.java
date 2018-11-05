a9f7abfbe5ca523ba96f0bbe0b42bfa37412d5b7
brooke <brookemperry@gmail.com>
2018-11-04 22:28:32 -0500

    worked on Editor Activity

--- app/src/main/java/com/example/android/brookesbooks/EditorActivity.java
+++ app/src/main/java/com/example/android/brookesbooks/EditorActivity.java
@@ -2,10 +2,17 @@ package com.example.android.brookesbooks;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.app.NavUtils;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -14,9 +21,9 @@ import android.widget.Toast;
 import com.example.android.brookesbooks.data.BooksContract.BookEntry;
 
 public class
-EditorActivity extends AppCompatActivity{
+EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
-   //EditText field for ISBN
+    //EditText field for ISBN
     private EditText mIsbnEditText;
 
     //EditText field for name of book
@@ -34,6 +41,12 @@ EditorActivity extends AppCompatActivity{
     //EditText for Supplier Phone number
     private EditText mSupplierPhoneEditText;
 
+    //Identifier for book data loader
+    private static final int EXISTING_BOOK_LOADER = 0;
+
+    //Content URI for existing book(null if it is a new book)
+    private Uri mCurrentBookUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,13 +54,16 @@ EditorActivity extends AppCompatActivity{
 
         //Get the intent and figure out which intent was used to launch the activity
         Intent intent = getIntent();
-        Uri currentBookUri = intent.getData();
+        mCurrentBookUri = intent.getData();
 
         //Determine whether we are creating a book (no URI) or editing an existing book (has URI)
-        if (currentBookUri == null) {
+        if (mCurrentBookUri == null) {
             setTitle(R.string.add_book_title);
         } else {
             setTitle(R.string.edit_book_title);
+
+            //Initialize the loader
+            getSupportLoaderManager().initLoader(EXISTING_BOOK_LOADER, null, this);
         }
 
         // Find all relevant views that we will need to read user input from
@@ -59,40 +75,73 @@ EditorActivity extends AppCompatActivity{
         mSupplierPhoneEditText = (EditText) findViewById(R.id.edit_supplier_phone_number);
     }
 
-     // Get user input from editor and save new book into database.
-     private void insertBook() {
+    // Get user input from editor and save a book into database.
+    private void saveBook() {
         // Read from input fields
         // Use trim to eliminate leading or trailing white space
         String isbnString = mIsbnEditText.getText().toString().trim();
         String nameString = mNameEditText.getText().toString().trim();
-         String priceString = mPriceEditText.getText().toString().trim();
-         int price = Integer.parseInt(priceString);
-         String quantityString = mQuantityEditText.getText().toString().trim();
-         int quantity = Integer.parseInt(quantityString);
+        String priceString = mPriceEditText.getText().toString().trim();
+        String quantityString = mQuantityEditText.getText().toString().trim();
         String supplierNameString = mSupplierNameEditText.getText().toString().trim();
         String supplierPhoneString = mSupplierPhoneEditText.getText().toString().trim();
+
+        //Check to see if all the values are empty. If so, just return w/out saving a new book
+        if (mCurrentBookUri == null &&
+                TextUtils.isEmpty(isbnString)&&
+                TextUtils.isEmpty(nameString)&&
+                TextUtils.isEmpty(priceString)&&
+                TextUtils.isEmpty(quantityString)&&
+                TextUtils.isEmpty(supplierNameString)&&
+                TextUtils.isEmpty(supplierPhoneString)){
+            return;
+        }
 
         // Create a ContentValues object where column names are the keys,
         // and pet attributes from the editor are the values.
         ContentValues values = new ContentValues();
         values.put(BookEntry.COLUMN_BOOK_ISBN, isbnString);
         values.put(BookEntry.COLUMN_BOOK_NAME, nameString);
+        int price = 0;
+        if (!TextUtils.isEmpty(priceString)){
+            price = Integer.parseInt(priceString);
+        }
         values.put(BookEntry.COLUMN_BOOK_PRICE, price);
-         values.put(BookEntry.COLUMN_BOOK_QUANTITY, quantity);
+        int quantity = 0;
+        if (!TextUtils.isEmpty(quantityString)){
+            quantity = Integer.parseInt(quantityString);
+        }
+        values.put(BookEntry.COLUMN_BOOK_QUANTITY, quantity);
         values.put(BookEntry.COLUMN_BOOK_SUPPLIER_NAME, supplierNameString);
         values.put(BookEntry.COLUMN_BOOK_SUPPLIER_PHONE, supplierPhoneString);
 
-        // Insert a new book into the provider & return content URI
-        Uri newUri = getContentResolver().insert(BookEntry.CONTENT_URI, values);
+        //Determine whether to add a book or update an existing book
+        if (mCurrentBookUri == null) {
+
+            // Insert a new book into the provider & return content URI
+            Uri newUri = getContentResolver().insert(BookEntry.CONTENT_URI, values);
 
 
-        // Show a toast message depending on whether or not the insertion was successful
-        if (newUri == null) {
-            // If the row ID is -1, then there was an error with insertion.
-            Toast.makeText(this, "Error with saving book", Toast.LENGTH_SHORT).show();
-        } else {
-            // Otherwise, the insertion was successful and we can display a toast with the row ID.
-            Toast.makeText(this, "Book saved successfully: ", Toast.LENGTH_SHORT).show();
+            // Show a toast message depending on whether or not the insertion was successful
+            if (newUri == null) {
+                // If the row ID is -1, then there was an error with insertion.
+                Toast.makeText(this, R.string.error_saving_book, Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the insertion was successful and we can display a toast with the row ID.
+                Toast.makeText(this, "Book saved successfully: ", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            //change existing book
+            int rowsAffected = getContentResolver().update(mCurrentBookUri,
+                    values,
+                    null,
+                    null);
+            //Show a toast message showing if the update was successful
+            if (rowsAffected == 0){
+                Toast.makeText(this,"Update to book failed", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Book updated successflly.", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
@@ -111,7 +160,7 @@ EditorActivity extends AppCompatActivity{
             // Respond to a click on the "Save" menu option
             case R.id.action_save:
                 // Save book to database
-                insertBook();
+                saveBook();
                 // Exit activity
                 finish();
                 return true;
@@ -126,5 +175,72 @@ EditorActivity extends AppCompatActivity{
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+        //The editor shows all attributes, so the projection must have all columns from the table
+        String[] projection = {
+                BookEntry._ID,
+                BookEntry.COLUMN_BOOK_ISBN,
+                BookEntry.COLUMN_BOOK_NAME,
+                BookEntry.COLUMN_BOOK_PRICE,
+                BookEntry.COLUMN_BOOK_QUANTITY,
+                BookEntry.COLUMN_BOOK_SUPPLIER_NAME,
+                BookEntry.COLUMN_BOOK_SUPPLIER_PHONE};
+
+        //This loader executes the ContentProvider's query method on a background thread
+        return new CursorLoader(this,
+                mCurrentBookUri,
+                projection,
+                null,
+                null,
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        //Exit if the cursor is null/there is <1 row in the curson
+        if (cursor == null || cursor.getCount() < 1) {
+            return;
+        }
+        //Go ahead to the first row of the cursor & read data from it
+        if (cursor.moveToFirst()) {
+            //Find the columns of book attributes we're concerned with
+            int isbnColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_ISBN);
+            int nameColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_NAME);
+            int priceColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_PRICE);
+            int quantityColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_QUANTITY);
+            int supplierNameColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_SUPPLIER_NAME);
+            int supplierPhoneColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_SUPPLIER_PHONE);
+
+            //Extract the value from the Cursor for the given column index
+            String isbn = cursor.getColumnName(isbnColumnIndex);
+            String name = cursor.getString(nameColumnIndex);
+            int price = cursor.getInt(priceColumnIndex);
+            int quantity = cursor.getInt(quantityColumnIndex);
+            String supplier_name = cursor.getString(supplierNameColumnIndex);
+            String supplier_phone = cursor.getString(supplierPhoneColumnIndex);
+
+            //Update the views on the screen with the values from the database
+            mIsbnEditText.setText(isbn);
+            mNameEditText.setText(name);
+            mPriceEditText.setText(Integer.toString(price));
+            mQuantityEditText.setText(Integer.toString(quantity));
+            mSupplierNameEditText.setText(supplier_name);
+            mSupplierPhoneEditText.setText(supplier_phone);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        //If the loader is invalidated, clear the data from the input fields
+        mIsbnEditText.setText("");
+        mNameEditText.setText("");
+        mPriceEditText.setText(0);
+        mQuantityEditText.setText(0);
+        mSupplierNameEditText.setText("");
+        mSupplierPhoneEditText.setText("");
     }
 }
